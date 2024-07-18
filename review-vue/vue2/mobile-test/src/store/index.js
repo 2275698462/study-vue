a690ec01c8a4f8da2faefb0103fdcf67b8431d6e
2275698462 <2275698462@qq.com>
2024-07-19 07:22:59 +0800

    复习 vuex

---
+++ review-vue/vue2/mobile-test/src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import cart from '@/store/modules/cart'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    cart
+  },
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {}
+})
